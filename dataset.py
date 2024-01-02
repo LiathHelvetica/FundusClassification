fae3f9827419b66cb62d609422269f5cc67fba05
liath <matthewdragun@gmail.com>
2024-01-02 23:04:03 +0100

    c5

--- dataset.py
+++ dataset.py
@@ -4,8 +4,7 @@ from torch.utils.data import Dataset
 from torchvision.io import read_image
 from torch import tensor, Tensor
 import torch
-import torch.nn.functional as F
-
+import torchvision.transforms as transforms
 
 from utils import get_id_from_file_name
 
@@ -25,7 +24,8 @@ class FundusImageDataset(Dataset):
     label_path: str,
     exclude_labels: set[str] | None = None,
     x_size: int = 224,
-    y_size: int = 224
+    y_size: int = 224,
+    force_same_class_representation: bool = True
   ):
     if exclude_labels is None:
       exclude_labels = []
@@ -42,15 +42,52 @@ class FundusImageDataset(Dataset):
       id = get_id_from_file_name(f_name)
       if id in self.label_df["ID"].values:
         self.images.append(f_name)
+    if force_same_class_representation:
+      max_n_repr = self.get_max_n_repr()
+      acc: dict[str, int] = dict()
+      override_out = []
+      for f_name in self.images:
+        id = get_id_from_file_name(f_name)
+        disease = self.get_label_by_id(id)
+        count = acc.get(disease)
+        if count is None:
+          override_out.append(f_name)
+          acc[disease] = 1
+        elif count <= max_n_repr:
+          override_out.append(f_name)
+          acc[disease] = acc[disease] + 1
+        else:
+          pass
+      self.images = override_out
 
-  def __len__(self):
-    len(self.images)
+  def get_max_n_repr(self) -> int:
+    acc: dict[str, int] = dict()
+    for f_name in self.images:
+      id = get_id_from_file_name(f_name)
+      disease = self.get_label_by_id(id)
+      if acc.get(disease) is not None:
+        acc[disease] = acc[disease] + 1
+      else:
+        acc[disease] = 1
+    counts = list(acc.values())
+    return 0 if len(counts) == 0 else max(counts)
+
+  def get_label_by_id(self, id) -> str:
+    return self.label_df[self.label_df["ID"] == id].iloc[0]["Disease"]
+
+  def __len__(self) -> int:
+    return len(self.images)
 
   def __getitem__(self, index):
     f_name = self.images[index]
     id = get_id_from_file_name(f_name)
     data = read_image(f"{self.img_path}/{f_name}")
-    return F.interpolate(data, size=(self.x_size, self.y_size)), self.label_df[self.label_df["ID"] == id].iloc[0]["Disease"]
+    resize_transform = transforms.Compose([
+      transforms.ToPILImage(),
+      transforms.Resize((self.x_size, self.y_size)),
+      transforms.ToTensor()
+    ])
+    return resize_transform(data), self.get_label_by_id(id)
 
   def labels(self) -> Tensor:
     return tensor(self.label_df["Disease"].unique(), dtype=torch.string)
--- utils.py
+++ utils.py
@@ -1,6 +1,3 @@
-from dataset import FundusImageDataset
-
-
 def flatten(l):
   return [item for sublist in l for item in sublist]
 
@@ -9,7 +6,7 @@ def get_id_from_file_name(f_name: str) -> int:
   return int(f_name.split("-")[-1].split(".")[0])
 
 
-def update_resizing(l: list[FundusImageDataset], x_size: int, y_size: int) -> None:
+def update_resizing(l: list, x_size: int, y_size: int) -> None:
   for dataset in l:
     dataset.x_size = x_size
     dataset.y_size = y_size
